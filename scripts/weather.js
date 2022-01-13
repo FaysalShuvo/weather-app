cbedcd4a7921ee00d602ba917ffd8099684ee11e
faysalshuvo <faysalshuvo.dev@gmail.com>
2022-01-13 14:45:59 +0600

    debug

--- scripts/weather.js
+++ scripts/weather.js
@@ -2,7 +2,7 @@ const apiKey = "ImznmLenxPveqQrPIWQPDpaxaq11YwkI";
 
 // get weather info
 const getWeather = async (locationKey) => {
-  const baseUrl = "http://dataservice.accuweather.com/currentconditions/v1/";
+  const baseUrl = "https://dataservice.accuweather.com/currentconditions/v1/";
   const query = `${locationKey}?apikey=${apiKey}`;
 
   const response = await fetch(baseUrl + query);
@@ -13,7 +13,7 @@ const getWeather = async (locationKey) => {
 // get location info
 const getCity = async (city) => {
   const baseUrl =
-    "http://dataservice.accuweather.com/locations/v1/cities/search";
+    "https://dataservice.accuweather.com/locations/v1/cities/search";
   const query = `?apikey=${apiKey}&q=${city}`;
 
   const response = await fetch(baseUrl + query);
