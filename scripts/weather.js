33016459f9dd964b33ceaf24a38034c9a025935c
faysalshuvo <faysalshuvo.dev@gmail.com>
2022-01-13 13:22:40 +0600

    getting weather info from api

---
+++ scripts/weather.js
@@ -0,0 +1,28 @@
+const apiKey = "ImznmLenxPveqQrPIWQPDpaxaq11YwkI";
+
+// get weather info
+const getWeather = async (locationKey) => {
+  const baseUrl = "http://dataservice.accuweather.com/currentconditions/v1/";
+  const query = `${locationKey}?apikey=${apiKey}`;
+
+  const response = await fetch(baseUrl + query);
+  const data = await response.json();
+  return data[0];
+};
+
+// get location info
+const getCity = async (city) => {
+  const baseUrl =
+    "http://dataservice.accuweather.com/locations/v1/cities/search";
+  const query = `?apikey=${apiKey}&q=${city}`;
+
+  const response = await fetch(baseUrl + query);
+  const data = await response.json();
+  return data[0];
+};
+
+getCity("sylhet")
+  .then((data) => getWeather(data.Key))
+  .then(data => console.log(data))
+  .catch((err) => console.log(err));
+
